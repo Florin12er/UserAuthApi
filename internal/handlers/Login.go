5491ce50f5ebcee63c6aa58ce415f43df95efc7b
Florin12er <apetreiflorin236@gmail.com>
2024-07-26 21:10:19 +0200

    added the ability to upload an thumbnails

--- internal/handlers/Login.go
+++ internal/handlers/Login.go
@@ -82,7 +82,7 @@ func Login(c *gin.Context) {
 		tokenString,
 		3600*24*30, // 30 days
 		"/",
-		"vercel.app", // Change this to your domain
+		"https://note-taking-dusky.vercel.app", // Change this to your domain
         true,
 		true,
 	)
